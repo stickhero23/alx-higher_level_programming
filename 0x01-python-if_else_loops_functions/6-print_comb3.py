e43d8991ba736618433f30d23091aba1763f4fa2
stickhero23 <stickhero23@gmail.com>
2023-01-06 00:41:43 +0000

    fixing 2 to 12

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(1, 10):
-        if (i < j or j == i):
+        if (j < i or j == i):
             continue
         if (i != 8):
             print("{:d}{:d}".format(i, j), end=', ')
