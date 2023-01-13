5a4b71ee3ebf451cbf4df6f11769412c60ba3fc7
stickhero23 <stickhero23@gmail.com>
2023-01-13 00:30:07 +0000

    fixed 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     nargs = len(sys.argv) - 1
-    if (nargs = 0):
+    if (nargs == 0):
         print("{:d} arguments.".format(nargs))
     elif (nargs == 1):
         print("{:d} argument:".format(nargs))
