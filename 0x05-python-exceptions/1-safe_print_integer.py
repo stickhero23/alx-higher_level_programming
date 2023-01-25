00d1635985d8a6a9e44f1258717ab879549e5eb0
stickhero23 <stickhero23@gmail.com>
2023-01-25 23:42:14 +0000

    fixing task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try :
-<<<<<<< HEAD
-        print("{:d}".format(int(value)))
-        return True
-    except:
-        return False
-=======
         print("{:d}".format(value))
+        return (True)
     except:
-        return False
-    else:
-        return True
->>>>>>> 33f292c37e90dc11706688931d374ee9bb126fea
+        return (False)
