8d9f20f8447f44b6a217b81e7bffa5445f50048c
stickhero23 <stickhero23@gmail.com>
2023-01-25 22:41:51 +0000

    fixed rask 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,4 +6,4 @@ def safe_print_division(a, b):
         inside_result = None
     finally:
         print("Inside result: {}".format(inside_result))
-    return (inside_result)
+        return (inside_result)
