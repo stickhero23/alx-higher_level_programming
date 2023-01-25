0de3b3e063367922e3a300e84ffbd451e3f2153c
stickhero23 <stickhero23@gmail.com>
2023-01-25 02:26:09 +0000

    adding task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             try:
                 print("{:d}".format(my_list[i]), end="")
+                count += 1
             except:
                 break
 
