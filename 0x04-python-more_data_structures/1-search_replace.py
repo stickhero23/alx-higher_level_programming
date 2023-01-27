103f85642ba77a7c5fea42e0d989f02de493cc26
stickhero23 <stickhero23@gmail.com>
2023-01-27 01:28:23 +0000

    adding fix in this

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is Not None:
-        return([x if x != search else replace for x in my_list])
-    return None
+    new_list = my_list.copy()
+    for i in range(len(my_list)):
+        if (my_list[i] == search):
+            new_list[i] = replace
+    return (new_list)
