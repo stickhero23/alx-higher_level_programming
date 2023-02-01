20ad416afd6e76b0af3aff0d2cf7c891cdb54fd9
stickhero23 <stickhero23@gmail.com>
2023-02-01 02:13:41 +0000

    adding changes

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Program prints a square with # on given input
+"""
+
+
+def print_square(size):
+    """ function prints a square using # symbol"""
+    msg = "size must be an integer"
