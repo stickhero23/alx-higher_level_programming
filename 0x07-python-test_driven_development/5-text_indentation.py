6d3fb06707d36a2d7e03cc7db2e4aa79dbb70e1b
stickhero23 <stickhero23@gmail.com>
2023-02-09 10:56:08 +0000

    adding task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
+"""
+Module 5-text_indentation
+Prints a text with 2 new lines after ., ?, and :
+"""
+def text_identation(text):
+    """
+    Prints text with 2 new lines after ".", "?", and ":"
+    """
+    if (type(text) is not str or ext is None):
+        raise TypeError("text must be a string")
+    flag = 0
+    for c in text:
+        if (c == '.' or c == '?' or c == ':'):
+            print(c, end='')
+            print('')
+            print('')
+            flag = 1
+        else:
+            if (flag == 0):
+                print(c, end='')
+            else:
+                if (c == ' ' or c == '\t'):
+                    pass
+                else:
+                    print(c, end="")
+                    flag = 0
 
