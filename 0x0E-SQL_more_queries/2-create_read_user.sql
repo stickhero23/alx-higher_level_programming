acba9e7ec9838d51def7ea01b0d339144c287c2f
stickhero23 <stickhero23@gmail.com>
2023-04-02 02:50:44 +0000

    adding task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -9,5 +9,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
 
 -- Grant select privilege
-GRANT SELECT PRIVILEGE ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT PRIVILEGE ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
