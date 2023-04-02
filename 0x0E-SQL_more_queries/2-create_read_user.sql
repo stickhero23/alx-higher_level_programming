deb3aa667ec1c25598d4dd7e76e5a00139e76040
stickhero23 <stickhero23@gmail.com>
2023-04-02 03:01:39 +0000

    adding task 3 and 4

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -12,6 +12,6 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
 GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
 
 -- Grant select privilege
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
 
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- create the table force_name
+-- has id INT and name VARCHAR(256) NOT NULL
+
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
